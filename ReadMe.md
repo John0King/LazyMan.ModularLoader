f5b8036e6e798e31c15d58d605d4c320cae39eb7
John0King <ABlueSky@outlook.com>
2020-05-31 13:51:27 +0800

    add readme and fix fallback order

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+﻿LayMan.ModularLoader
+=========================
+
+一个方面的插件加载器，适用于.net core , 支持插件程序集隔离，和插件依赖， 支持加载原生库
+
+> 该库目前处于非常早期的状态，请勿用于生产环境
+
+## 项目结束
+
+### 1. LazyMan.ModularLoader
+
+基础的插件加载器，这里面所有的类都是抽象的，要想使用需要里面里面`HostLoader`来 自己封装自己的加载器
+
+
+### 2. LazyMan.ModularLoader.AspNetCore
+
+asp.net core 的 子应用程序（SubApp） 加载器，目的是将任意的现有基于asp.net core的web应用，以完全隔离的方式
+加载当前进程，并且映射到子路径上（注意，静态html文件和js文件路径可能出现错误）
+
+
+## 捐赠 Donate
+
+手头富裕的可以支持我将此项目继续下去i😜， 一毛不嫌少， 一万不嫌多 哈哈 
+
+![wechat](./docs/assets/donate_wechat.png)
+![alipay](./docs/assets/donate_alipay.jpg)
--- src/LazyMan.ModularLoader/Internal/PluginAssemblyLoadContext.cs
+++ src/LazyMan.ModularLoader/Internal/PluginAssemblyLoadContext.cs
@@ -19,19 +19,41 @@ namespace LazyMan.ModularLoader.Internal
             ALCContext = alcContext ?? throw new ArgumentNullException(nameof(alcContext));
             Resolver = new AssemblyDependencyResolver(info.PluginDll);
         }
-
+        /// <summary>
+        /// the <see cref="PluginInfo"/>
+        /// </summary>
         public PluginInfo PluginInfo { get; }
+
+        /// <summary>
+        /// the context for this plugin
+        /// </summary>
         PluginContext ALCContext { get; }
+
+        /// <summary>
+        /// the dependencyReolver
+        /// </summary>
         AssemblyDependencyResolver Resolver { get; }
+
+        /// <inheritdoc />
         protected override Assembly? Load(AssemblyName assemblyName)
         {
             Assembly? a = null;
-            //a = ALCContext.HostLoadContext.LoadFromAssemblyName(assemblyName);
-            a = ALCContext.SharedAssemblies.Where(x=>AssemblyName.ReferenceMatchesDefinition(x.GetName(),assemblyName)).FirstOrDefault();
+            // if the assembly is in SharedAssembies then we use that assembly first
+            a = ALCContext.SharedAssemblies
+                .Where(x=>AssemblyName.ReferenceMatchesDefinition(x.GetName(),assemblyName))
+                .FirstOrDefault();
             if (a != null)
             {
                 return a;
             }
+
+            // if the assembly is in dependented plugins 
+            // then we use the assembly in the assembly in dependented plugin
+            // ========= [mark]  =============================
+            // should we only use the plugin's entryAssemlby? 
+            // or we add [ExportedAssemlby] in PluginInfo
+            // or we use some attribute like [ExportAttribute] to detect all it's dependented assembly
+            // ===============================================
             foreach (var alc in ALCContext.Plugins)
             {
                 a = alc.Value.LoadFromAssemblyName(assemblyName);
@@ -41,12 +63,26 @@ namespace LazyMan.ModularLoader.Internal
                 }
             }
 
+
+            // load the dependency from our only
             var path = Resolver.ResolveAssemblyToPath(assemblyName);
             if (path != null)
             {
                 return LoadFromAssemblyPath(path);
             }
 
+
+            // fallback to local folder first
+
+            System.Diagnostics.Debug.WriteLine(assemblyName);
+
+            var file = PluginInfo.PluginFolder + assemblyName.Name + ".dll";
+            if (File.Exists(file))
+            {
+                return LoadFromAssemblyPath(file);
+            }
+
+            // fallback to Host AssemlbyLoadContext
             a =  ALCContext.HostLoadContext.LoadFromAssemblyName(assemblyName);
             if(a != null)
             {
@@ -54,21 +90,14 @@ namespace LazyMan.ModularLoader.Internal
             }
            
 
-            // razor issue
-
-            System.Diagnostics.Debug.WriteLine(assemblyName);
-
-            var file = PluginInfo.PluginFolder + assemblyName.Name + ".view.dll";
-            if (File.Exists(file))
-            {
-                return LoadFromAssemblyPath(file);
-            }
+            // fallback to AssemblyLoadContext.Default
 
             return base.Load(assemblyName);
         }
 
 
-        // native ?  do we need any other handle method ?
+        /// <inheritdoc/>
+        /// <remarks> native ?  do we need any other handle method ?</remarks>
         protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
         {
             var path = Resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
